4decf2714b6d1eaf19270dd15dac2b07467bddbf
3woods <3woods@163.com>
2017-09-19 23:46:48 +0800

    first

---
+++ src/main/java/com/woods/boot_repo/BootRepoApplication.java
@@ -0,0 +1,11 @@
+package com.woods.boot_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRepoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootRepoApplication.class, args);
+	}
+}
