2e80f2250611a53b3e298de0391492980c51cc2c
3woods <3woods@163.com>
2017-09-20 13:22:51 +0800

    add

--- src/main/java/com/woods/boot_repo/controller/UserController.java
+++ src/main/java/com/woods/boot_repo/controller/UserController.java
@@ -1,33 +1,33 @@
-package com.neo.controller;
+package com.woods.boot_repo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.woods.boot_repo.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.neo.domain.User;
-import com.neo.domain.UserRepository;
-
 @RestController
 public class UserController {
 
 	@Autowired
-	private UserRepository userRepository;
+//	private UserRepository userRepository;
 
     @RequestMapping("/getUser")
     @Cacheable(value="user-key")
-    public User getUser() {
-    	User user=userRepository.findByUserName("aa");
+    public UserEntity getUser() {
+//        UserEntity user=userRepository.findByUserName("aa");
+        UserEntity user=new UserEntity();
     	System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
         return user;
     }
 
     @RequestMapping("/getUsers")
     @Cacheable(value="key-Users")
-    public List<User> getUsers() {
-    	List<User> users=userRepository.findAll();
+    public List<UserEntity> getUsers() {
+    	List<UserEntity> users=new ArrayList<>();//userRepository.findAll();
     	System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
         return users;
     }
